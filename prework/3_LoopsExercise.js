fdfade54d599c8f8791b55886d78c4dc5fd71c1f
Shaylan7 <80442267+Shaylan7@users.noreply.github.com>
2021-06-30 21:45:55 -0700

    Shay 3_LoopsExercise.js

--- prework/3_LoopsExercise.js
+++ prework/3_LoopsExercise.js
@@ -11,30 +11,106 @@
 
 // Exercise 1. Write a 'while' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
-
+var x = 1; 
+while (x < 5){
+  console.log(x);
+ x+=1 
+} 
 // Exercise 2. Write a 'do while' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
+var x =1;
+do {
+  console.log(x)
+} while (x<5); 
 
 // Exercise 3. Write a 'for' loop that prints the integers (whole numbers)
 // between 1 and 5 (inclusive).
+var x =1;
+do {
+  console.log(x);
+  x++;
+} while (x<=5); 
 
 // Exercise 4. Now we want a loop that prints the integers
 // counting DOWN from 10 to 1 (inclusive). Write three loops that do this, to practice
 // writing loops in all 3 ways -- as a 'while', 'do while', and 'for' loop.
 
+// while
+var x = 10; 
+while (x > 1){
+  console.log(x);
+ x-=1
+}
+
+// do while 
+var x = 10;
+do {
+  console.log(x);
+  x = x-1;
+} while (x > 1); 
+
+// for loop 
+
+for (var x = 10; x > 0; x--) {
+  console.log(x);
+}
+
 // Exercise 5. Write a loop that prints the integers from 7 to 27. Write this
 // loop in all 3 ways -- as a 'while', 'do while', and 'for' loop.
+
+//while 
+var x = 7; 
+while (x >= 26){
+  console.log(x);
+ x+=1
+}
+
+//do while
+
+var x = 7;
+do {
+  console.log(x);
+  x = x+1;
+} while (x < 27); 
+
+//for 
+for (var x = 7; x <= 27; x++) {
+  console.log(x);
+}
+
 
 // Exercise 6. Write a loop that prints numbers between 0 and 100, counting by tens.
 // I.e. it will print 10, then 20, then 30, etc.
 // Write this loop in all 3 ways -- as a 'while', 'do while', and 'for' loop.
 
+//while 
+var x = 0; 
+while (x < 100){
+  console.log(x);
+ x+=10
+}
+
+//do while
+
+var x = 0;
+do {
+  console.log(x);
+  x = x+10;
+} while (x < 100); 
+
+//for 
+for (var x = 0; x <= 100; x++) {
+  console.log(x);
+}
+
+
 // Exercise 7. Add a comment as to why the following loop is an infinite loop (will
 // run without ever stopping). Then fix the loop so that it stops when
 // counterFour is equal to -100.
+//The counter starts at 1 which is less than 2. Then the loop decrements from the starting point, therefore counterFour will always be less than 2. 
 
 let counterFour = 1;
-while (counterFour < 2) {
+while (counterFour >= -100) {
   console.log("HELP ME!");
   counterFour--;
 }
@@ -42,6 +118,9 @@ while (counterFour < 2) {
 // Exercise 8. Make a variable that contains your favorite integer. Write a loop
 // (your choice which type) that prints the integers from 0 to that number.
 
+for (var x =0; x <=7; x++) {
+console.log(x);
+}
 // Exercise 9. Make a variable that contains your favorite integer (this time make sure it's
 // less than 100). Write a loop (your choice which type) that prints the integers from 0 to 100.
 // Next to each number it should print "not my favorite number". But next to your favorite
@@ -52,12 +131,21 @@ while (counterFour < 2) {
 // 3 not my favorite number
 // ...
 // (Hint - use an if statement in your loop)
+var favoriteNumber = 7;
+for (var x =0; x <=100; x++) {
+  if (x == favoriteNumber) {
+    console.log(x + " my favorite number!");
+} else console.log(x + " not my favorite number"); 
+}
 
 // Exercise 10. In some of the exercises above, we had you write all 3 types of loops, for practice.
 // But in real life, how would you decide which type of loop to use? You might not have an
 // exact answer, but spend a few minutes thinking about the different types of loops and/or
 // doing some research and write down your thoughts in a comment below:
-//
+//For brevity I would chose the for loop and if I know how many times I want the code to execute. I would use a do 
+//while loop if I want the code to execute at least once. For example, it could be used with a user inputing an answer.
+// We would want the computer to print out something if the user entered something incorrect. A while loop is if we 
+// only want something to print if the criteria is met. 
 
 // Exercise 11. Now we'll practice using nested loops (a loop inside another loop)!
 // Update the nested loops below to so that for each value of outsideCounter,
